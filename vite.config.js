d9bebc695d9cebec280873922d205a8c6958de3c
dimasphp999-droid <dimasphp999@gmail.com>
2026-09-05 00:52:32 +0700

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/flowfilm/', // SAMAKAN DENGAN NAMA REPOSITORI ANDA
+  base: '/flowfilm/', 
 })
